7b23c7041e9cdc8a3e7c87a7a5aaa4b03a5a4d96
devendra dhare <devendradhare03@gmail.com>
2022-07-28 13:07:25 +0530

    this is past

--- js/custom.js
+++ js/custom.js
@@ -11,15 +11,7 @@ getYear();
 /** google_map js **/
 function myMap() {
     var mapProp = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        center: new google.maps.LatLng(21.996, 78.807),
-=======
-        center: new google.maps.LatLng(78.712775, 21.005973),
->>>>>>> parent of 4b5ea58 (google map location fix)
-=======
-        center: new google.maps.LatLng(78.712775, 21.005973),
->>>>>>> parent of 4b5ea58 (google map location fix)
+        center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
